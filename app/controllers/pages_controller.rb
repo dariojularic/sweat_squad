dd37d0f4c2c8194d094164c44026900858a226f8
Robert Fehlhaber <rob.fehlhaber@gmail.com>
2023-03-10 10:39:26 +0100

    Loads of updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @user = current_user
+    if @user
+    @sport_events = @user.sport_events
+    end
   end
 
   def dashboard
