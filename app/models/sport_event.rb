3a14d720795d705e585cd408152224891cb42084
Dario Jularic <dariojularic0@gmail.com>
2023-03-10 16:07:38 +0100

    ratings show

--- app/models/sport_event.rb
+++ app/models/sport_event.rb
@@ -2,7 +2,7 @@ class SportEvent < ApplicationRecord
   SPORTS = %w[futsal basketball squash football tennis volleyball badminton table-tennis]
   belongs_to :user
   has_many :users, through: :requests
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :requests, dependent: :destroy
   validates :address, presence: true
